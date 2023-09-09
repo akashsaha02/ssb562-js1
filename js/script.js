be7443f6a2d8f0d83ddf5d0c0724ab1e13ee6163
Akash4056 <akashsaha1313@gmail.com>
2023-09-09 19:41:13 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// text-change
+
+function textChange() {
+
+	document.getElementById('h2').innerHTML="Hello World"
+}
+//text-change
+function wordChange(){
+
+	document.getElementById('h3').innerHTML="Hello World";
+}
+//text-change
+function docChange(){
+
+	document.getElementById('h4').innerHTML="Hello World";
+}
+//text-change
+function forChange(){
+
+	document.getElementById('h5').innerHTML="Hello World";
+}
+//text-change
+function classChange(){
+
+	document.getElementById('h6').innerHTML="Hello World";
+}
+//text-change
+function scoChange(){
+
+	document.getElementById('h7').innerHTML="Hello World";
+}
+//text-change
+function sucChange(){
+
+	document.getElementById('h8').innerHTML="Hello World";
+}
+//text-change
+function rimChange(){
+
+	document.getElementById('h9').innerHTML="Hello World";
+}
+//text-change
+function infChange(){
+
+	document.getElementById('h10').innerHTML="Hello World";
+}
+// text change 
+function danChange(){
+	document.getElementById('h11').innerHTML="Hello World"
+}
